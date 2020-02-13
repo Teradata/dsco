3fed3cf09d2461dac29a1202d58c7d0b3079a663
Aaron Palumbo <aaron.palumbo@teradata.com>
2020-02-13 14:38:21 -0800

    add debug to up

--- dsco/subparsers/up.py
+++ dsco/subparsers/up.py
@@ -10,26 +10,33 @@ def add_cmd(subparsers):
     cmd = subparsers.add_parser(cmd_name, help="create and launch containers")
     cmd.add_argument("--dev", action="store_true", help="dev container only")
     cmd.add_argument("--prod", action="store_true", help="prod container only")
+    cmd.add_argument(
+        "--debug",
+        action="store_true",
+        help="bring up base container without running ansible",
+    )
     cmd.add_argument("--all", action="store_true", help="dev and prod containers")
 
 
 def run_cmd(args, conf):
     if conf["proj_root"]:
-        dev_flag = args.dev or args.all
-        prod_flag = args.prod or args.all
-
-        # services_conf = conf["docker_compose_yaml"]["services"]
+        # Flag logic
+        no_flags = not args.dev and not args.prod and not args.all and not args.debug
+        dev_flag = (args.dev or args.all or no_flags) and not args.debug
+        prod_flag = (args.prod or args.all) and not args.debug
+        debug_flag = args.debug
 
         if prod_flag:
             cmd = "docker-compose up -d prod"
             subprocess.run(cmd, cwd=conf["proj_root"], shell=True)
-            # proj_port = services_conf["prod"]["ports"][0].split(":")[0]
 
-        only_prod_flag = not dev_flag and prod_flag
-        if not only_prod_flag:
+        if dev_flag:
             cmd = "docker-compose up -d dev"
             subprocess.run(cmd, cwd=conf["proj_root"], shell=True)
-            # proj_port = services_conf["dev"]["ports"][0].split(":")[0]
+
+        if debug_flag:
+            cmd = "docker-compose up -d debug"
+            subprocess.run(cmd, cwd=conf["proj_root"], shell=True)
 
     else:
         print("No project found.")
